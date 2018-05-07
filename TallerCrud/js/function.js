71da1b7de4daaddbe3371f2c51e8988157d52ab4
luisrj1495 <luisrj1495@gmail.com>
2018-05-06 20:21:18 -0500

    finished crud js y php

--- TallerCrud/js/function.js
+++ TallerCrud/js/function.js
@@ -1,24 +1,137 @@
-const validateInputs = (ev) => {
+const insertData = (ev) => {
     let elements = document.getElementById('frmInsert')
-    let formData = new FormData(); 
-
-    for(let i=0; i<elements.length; i++){
-
-        formData.append(elements[i].name, elements[i].value)
+    let data = {
+        name:  document.getElementsByName('nameInput')[0].value,
+        age: document.getElementsByName('ageInput')[0].value,
+        tel: document.getElementsByName('telInput')[0].value,
+        action: ev.target.name
     }
-    formData.append('action', ev.target.name)
-    loadData("info",formData )
+    loadData(JSON.stringify(data), (res) => {
+        document.getElementById('infoSelect').innerHTML = res
+        setTimeout(() => {
+            document.getElementById('infoSelect').innerHTML = ''
+        }, 4000)
+        
+    })
+    
     elements.reset()
 }
 
+const checkUser = (ev) => {
+    let data = {
+        id: ev.target.value,
+        action: 'select',
+    }
+    let resJson = {}
+    
+    if(data.id == ''){
+        document.getElementsByName('nameUpdate')[0].value = ''
+        document.getElementsByName('ageUpdate')[0].value = ''
+        document.getElementsByName('telUpdate')[0].value = ''
+        return false
 
-const loadData = (inputShow, params) => {
-    let xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById(inputShow).innerHTML = this.responseText
+
+    } else {
+        loadData(JSON.stringify(data),(res) => {
+            if (res !='0') {            
+                resJson = JSON.parse(res)
+                document.getElementsByName('nameUpdate')[0].value = resJson.name
+                document.getElementsByName('ageUpdate')[0].value = resJson.age
+                document.getElementsByName('telUpdate')[0].value = resJson.tel
+            }     
+        })
+        return true
+    }
+
+}
+
+const countData = () => {
+
+    let data = {
+        action: 'countData'
+    }
+   
+    loadData(JSON.stringify(data), (res) => {
+       document.getElementById('counter').innerHTML = res
+        
+    })
+
+    
+    
+}
+
+const update = (ev) => {
+    console.log(ev.target.name);
+    
+    let data = {
+        action: ev.target.name,
+        id: document.getElementById('idUpdate').value,
+        name:  document.getElementsByName('nameUpdate')[0].value,
+        age: document.getElementsByName('ageUpdate')[0].value,
+        tel: document.getElementsByName('telUpdate')[0].value,
+       
+    }    
+    loadData(JSON.stringify(data), (res) => {
+        document.getElementById('infoUpdate').innerHTML = res
+        setTimeout(() => {
+            document.getElementById('infoUpdate').innerHTML = ''
+        }, 10000)
+        
+    })
+
+}
+
+const deleted = (val) =>{
+    let message = prompt("Desea eliminar el registro ( si / no)??");
+    if(message != null && message.toLowerCase() == 'si'){
+        let data = {
+            action: 'delete',
+            id: val
+        }
+
+        loadData(JSON.stringify(data), (res) => {
+            alert(res)
+            document.getElementById('tbody').innerHTML = ''
+            selectAll()
+        })
+        
+    }
+    
+} 
+
+const selectAll = () =>{
+    let data = {
+        action : 'selectAll'
+    }
+    loadData(JSON.stringify(data), (res) => {
+        resJson = JSON.parse(res)
+        resJson.map((val) => {
+            document.getElementById('tbody').innerHTML += `<tr>
+             <td>${val.id}</td> 
+             <td>${val.name}</td>
+             <td>${val.tel}</td>
+             <td>${val.age}</td>
+             <td><a  class="btn-floating btn-large waves-effect waves-light red" onClick="deleted(${val.id})">
+                <i class="material-icons right">delete</i>
+                </a> 
+            </td>
+             </tr>`
+        } )        
+    })
+}
+
+
+const loadData = (params, cb) => {
+     let xhttp = new XMLHttpRequest()
+     xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) { 
+        cb(this.responseText)
+   
       }
     }
+    
     xhttp.open('POST', './php/controller.php', true)
-    xhttp.send(params)
+    xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhttp.send(params) 
+  
 }
--- TallerCrud/php/controller.php
+++ TallerCrud/php/controller.php
@@ -1,25 +1,111 @@
 <?php
-require_once('conection.php');
+include('conection.php');
 
-$action = $_POST['action'];
+/// Obtenemos el json enviado
+header("Content-Type: application/json");
+// build a PHP variable from JSON sent using POST method
+$data = json_decode(stripslashes(file_get_contents("php://input")),true);
+
+$action = "";
+$action = $data['action'];
 
 switch ($action) {
     case 'send':
-        $name = $_POST['nameInput'];
-        $tel = $_POST['telInput'];
-        $age = $_POST['ageInput'];
+        $name = $data['name'];
+        $tel = $data['tel'];
+        $age = $data['age'];
         $sql = "INSERT INTO usercrud (name,age,tel) VALUES ('$name',$age,$tel);";
         try{
             $conect->query($sql);
             echo "<span class='red-text text-darken-2'> Succes <i class='material-icons'>check</i> </span>";
         } catch (Exception $e) {
             echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+        }
+
+        break;
+    case 'select':
+        $id = $data['id'];
+        $sql = "SELECT * FROM usercrud WHERE id=$id;";
+        $result = $conect->query($sql);
+        try{
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    $rowInfo = array('id' => $row["id"], 'name' => $row["name"], 'age' => $row["age"],
+                    'tel' => $row["tel"] );
+                    echo json_encode($rowInfo);
+                }
+            } else {
+                echo "0";
+            }
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ',  $e->getMessage(), "\n";
         } 
         break;
-    
-    default:
-        # code...
+    case 'update':
+        $id = $data['id'];
+        $name = $data['name'];
+        $tel = $data['tel'];
+        $age = $data['age'];
+        $sql = "UPDATE usercrud SET name = '$name', age = $age, tel = $tel WHERE id = $id;";   
+        try{
+            if ($conect->query($sql)) {
+               echo "<span class='red-text text-darken-2'> Succes Updating <i class='material-icons'>check</i> </span>";
+
+            }else{
+                echo "<span class='red-text text-darken-2'> Not Succes Updating <i class='material-icons'>error</i> </span>";
+            } 
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+        } 
         break;
+    case 'selectAll':
+        $sql = "SELECT * FROM usercrud;";
+        $data = array();
+        $result = $conect->query($sql);
+
+        try{
+            if ($result->num_rows > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+
+                    $data[] = $row;
+                   
+                }
+                echo json_encode($data);
+                
+                
+            } else {
+                echo "0";
+            }
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+        } 
+        break;
+    case 'delete':
+        $id = $data['id'];
+        $sql = "DELETE FROM usercrud WHERE id = $id;";
+        try{
+            $conect->query($sql);
+            echo "Deleted";
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+        } 
+        
+    break;
+
+    case 'countData':
+        $sql = "SELECT COUNT(*) AS cant FROM usercrud;";
+        $result = $conect->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            $row = $result->fetch_assoc();
+              
+                echo $row["cant"];
+            
+        } else {
+            echo "0";
+        }
+
 }
 
 
